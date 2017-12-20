e3dcef6e13e908fdcb4fc93013e5418aa58b4ccf
Luke Charles <lukecharles222@gmail.com>
2017-12-20 12:02:24 +0000

    question library added

--- app/src/main/java/laminarsoftworks/mathsquiz/QuizActivity.java
+++ app/src/main/java/laminarsoftworks/mathsquiz/QuizActivity.java
@@ -2,8 +2,22 @@ package laminarsoftworks.mathsquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
